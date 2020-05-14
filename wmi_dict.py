8c02f6385c6f30dfb3ad8c9f1a532a14e3ae5c1f
Petronilo Bion <petroniloneto.am@hotmail.com>
2020-05-14 11:20:47 -0300

    Adding Chipset Information

--- wmi_dict.py
+++ wmi_dict.py
@@ -12,6 +12,7 @@ WMI_DICT = {
     "Placa Mãe": [],
     "Bios": [],
     "Sistema Operacional": [],
+    "Chipset:": [],
 }
 
 list_of_classes = ["Win32_Processor", "Win32_DesktopMonitor", "Win32_PhysicalMemory", "Win32_VideoController", "Win32_DiskDrive", "Win32_Keyboard", "Win32_PointingDevice", "Win32_Battery", "Win32_BaseBoard", "Win32_BIOS", "Win32_OperatingSystem"]
@@ -19,5 +20,10 @@ i = 0
 for device in WMI_DICT:
     for n in wmi.WMI().query("Select * From " + list_of_classes[i]):
         for m in n._properties:
-            WMI_DICT[device].append({"propriedade": m, "query": "Select " + m + " From " + list_of_classes[i], "value": None})
+            if device == "Chipset":
+                WMI_DICT[device].append({"propriedade": m, "query": "Select " + m + " From " + list_of_classes[i] + " Where Caption Like '%Chipset%'", "value": None})
+            else:
+                WMI_DICT[device].append({"propriedade": m, "query": "Select " + m + " From " + list_of_classes[i], "value": None})
+
     i += 1
+
